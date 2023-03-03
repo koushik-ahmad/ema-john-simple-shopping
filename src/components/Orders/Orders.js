1f85852242d835deac79e3c07a7b44e25fa60048
koushik-ahmad <koushikahmad123@gmail.com>
2023-03-03 16:52:03 +0600

    added orders

---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+
+const Orders = () => {
+    const { products, initialCart } = useLoaderData(); // { products: products, initialCart: initialCart }
+    const [cart, setCart] = useState(initialCart);
+
+    return (
+        <div>
+            <div className="shop-container">
+                <div className="products-container">
+
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Orders;
